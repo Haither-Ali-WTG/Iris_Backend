a0cfe99e6bb38d874328e1d32724d27a86db55bf
Slav <slav.simdyankin@wisetechglobal.com>
2022-09-26 15:09:13 +1000

    Upload servers data to mongo

--- ansible/roles/collect_sites/files/import_to_mongo.py
+++ ansible/roles/collect_sites/files/import_to_mongo.py
@@ -17,13 +17,13 @@ from pymongo import MongoClient
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--servers_config_wildcard', type=str, default='servers/*.json')
+    parser.add_argument('--servers_config_wildcard', type=str, default='servers/*.yml')
     parser.add_argument('--host', type=str, default="lethe.dante.wtg.ws")
     parser.add_argument('--port', type=int, default=27017)
     parser.add_argument('--username', '-u', type=str, default="charon")
     parser.add_argument('--password', '-p', type=str)
     parser.add_argument('--db', type=str, default="charon")
-    parser.add_argument('--replicaSet', default="lethe")
+    parser.add_argument('--replicaSet', default="au2-lethe-a")
     parser.add_argument('--authenticationDatabase', type=str, default="admin")
     parser.add_argument('--collection', type=str,
                         default="default-haproxy-collection")
@@ -45,34 +45,38 @@ def main():
 
     start_time = datetime.now()
 
-    # with MongoClient("mongodb://{0}:{1}/".format(args.host, args.port),
-    #                  username=args.username,
-    #                  password=args.password,
-    #                  authSource=args.authenticationDatabase,
-    #                  w="majority",
-    #                  replicaSet=args.replicaSet) as connection:
+    with MongoClient("mongodb://{0}:{1}/".format(args.host, args.port),
+                     username=args.username,
+                     password=args.password,
+                     authSource=args.authenticationDatabase,
+                     w="majority",
+                     replicaSet=args.replicaSet) as connection:
 
-    #     db = connection[args.db]
-    #     collection = db[args.collection]
+        db = connection[args.db]
+        collection = db[args.collection]
 
-    #     config_files = glob(args.servers_config_wildcard)
+        config_files = glob(args.servers_config_wildcard)
 
-    #     for config in config_files:
-    #         server_name = path.basename(config)
-    #         server_name = path.splitext(server_name)[0]
+        for config in config_files:
+            server_name = path.basename(config)
+            server_name = path.splitext(server_name)[0]
 
-    #         with codecs.open(config, 'r', 'utf-8-sig') as handler:
-    #             configuration = json.load(handler)
-    #             configuration['server_name'] = server_name
-    #             try:
-    #                 configuration['ip'] = socket.gethostbyname(server_name)
-    #             except socket.gaierror:
-    #                 configuration['ip'] = 'unresolved'
-    #                 logger.info("Unable to resolve hostname: %s", server_name)
+            server_data = dict()
+            with open(config, 'r') as stream:
+                try:
+                    server_data['services'] = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    print(exc)
 
-    #             collection.replace_one({"server_name": server_name}, configuration, upsert=True)
-    #             logger.info('Server data uploaded: %s', server_name)
+            server_data['server_name'] = server_name
+            try:
+                server_data['ip'] = socket.gethostbyname(server_name)
+            except socket.gaierror:
+                server_data['ip'] = 'unresolved'
+                logger.info("Unable to resolve hostname: %s", server_name)
 
+            collection.replace_one({"server_name": server_name}, server_data, upsert=True)
+            logger.info('Server data uploaded: %s', server_name)
 
     logger.info("Execution duration: %s", str(datetime.now() - start_time))
     return 0
