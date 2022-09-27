dd9c93e30988bd16f5554e527894517505f58ca1
Slav <slav.simdyankin@wisetechglobal.com>
2022-09-27 14:27:07 +1000

    lynts errors

--- ansible/roles/collect_sites/files/collect_sites_data.py
+++ ansible/roles/collect_sites/files/collect_sites_data.py
@@ -47,6 +47,9 @@ def get_websites_with_parameters(sites_list, website_config, logger):
             if (site_name.startswith(tuple(config['starts_with']))
                  or any(like in site_name for like in config['name_like'])):
                 websites[site_name] = config
+            else:
+                logger.info('%s: Site name does not match any config. Skipping................',
+                            site_name)
 
     return websites
 
