889f9083140a0098d5ae3f14e3e0f5c4ef47fc89
Slav <slav.simdyankin@wisetechglobal.com>
2023-01-09 11:15:29 +1100

    Support several configs for website

--- ansible/roles/collect_sites/files/collect_sites_data.py
+++ ansible/roles/collect_sites/files/collect_sites_data.py
@@ -48,8 +48,10 @@ def get_websites_with_parameters(sites_list, website_config, server_ip, logger):
         for config in website_config:
             if (site_name.startswith(tuple(config['starts_with']))
                  or any(like in site_name for like in config['name_like'])):
+                if site_name not in websites:
+                    websites[site_name] = []
                 config['server_ip'] = server_ip
-                websites[site_name] += [config]
+                websites[site_name].append(config)
                 name_matched_times += 1
 
         if name_matched_times == 0:
