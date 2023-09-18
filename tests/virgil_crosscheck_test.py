05e66e256374d9fc6c59d84a7f5b1f2ca6b4ec43
Jiri Baum <Jiri.Baum@wisetechglobal.com>
2023-09-19 09:35:47 +1000

    Neaten up with black

--- tests/virgil_crosscheck_test.py
+++ tests/virgil_crosscheck_test.py
@@ -112,27 +112,31 @@ class TestVirgilCrosscheck(TestCase):
                             virgil_vlans,
                             too_long=ValueError(
                                 f"Machines across multiple VLANs: {sorted(virgil_vlans)}"
-                            )
+                            ),
                         )
                         virgil_subnet = IPv4Network(subnet)
 
                     for floating_ip in config["floating_ips"]:
                         self.assertEqual(
-                            floating_ip['vlan'], virgil_vlan,
+                            floating_ip["vlan"],
+                            virgil_vlan,
                             f"Floating IP VLAN {floating_ip['vlan']} "
-                            f"does not match machine VLAN {virgil_vlan}"
+                            f"does not match machine VLAN {virgil_vlan}",
                         )
                         self.assertIn(
-                            IPv4Address(floating_ip['ip']), virgil_subnet,
+                            IPv4Address(floating_ip["ip"]),
+                            virgil_subnet,
                             f"Floating IP {floating_ip['ip']} "
-                            f"is not in the VLAN subnet {virgil_subnet}"
+                            f"is not in the VLAN subnet {virgil_subnet}",
                         )
                         self.assertCountEqual(
-                            floating_ip['priority'], set(floating_ip['priority']),
-                            f"Priority list has repeats: {floating_ip['priority']}"
+                            floating_ip["priority"],
+                            set(floating_ip["priority"]),
+                            f"Priority list has repeats: {floating_ip['priority']}",
                         )
                         self.assertLessEqual(
-                            set(floating_ip['priority']), set(config["machines"]),
+                            set(floating_ip["priority"]),
+                            set(config["machines"]),
                             "Priority list has machines not in cluster: "
-                            "{set(floating_ip['priority']) - set(config['machines'])}"
+                            "{set(floating_ip['priority']) - set(config['machines'])}",
                         )
